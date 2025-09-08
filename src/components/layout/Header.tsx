110dfce9945bf369cc9143b3371f63618fdc72df
IrfanAhmed651 <irfanahmed651@gmail.com>
2025-09-08 10:46:48 +0600

    added scroll animation

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+import { Link, animateScroll as scroll } from "react-scroll";
+
 export function Header() {
   // function closeDrawer() {
   //   const el = document.getElementById("mobile-drawer");
@@ -8,29 +11,60 @@ export function Header() {
   return (
     <header className="sticky top-0 z-30 w-full bg-white/70 dark:bg-black/70 backdrop-blur-md border-b border-gray-200 dark:border-gray-800">
       <nav className="max-w-6xl mx-auto flex justify-between items-center px-6 py-4">
-        <div className="font-bold text-xl bg-gradient-to-r from-indigo-600 to-pink-600 bg-clip-text text-transparent">
+        <div
+          onClick={() => scroll.scrollToTop({ duration: 600, smooth: true })}
+          className="cursor-pointer font-bold text-xl bg-gradient-to-r from-indigo-600 to-pink-600 bg-clip-text text-transparent"
+        >
           It Clone
         </div>
         <ul className="hidden md:flex gap-8 text-gray-700 dark:text-gray-200 font-medium">
           <li>
-            <a href="#about" className="hover:text-indigo-600">
+            <Link
+              to="about"
+              smooth={true}
+              offset={-80}
+              duration={600}
+              spy={true}
+              className=" cursor-pointer hover:text-indigo-600"
+            >
               About
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="#services" className="hover:text-indigo-600">
+            <Link
+              to="services"
+              smooth={true}
+              offset={-80}
+              duration={600}
+              spy={true}
+              className="cursor-pointer hover:text-indigo-600"
+            >
               Services
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="#portfolio" className="hover:text-indigo-600">
+            <Link
+              to="portfolio"
+              smooth={true}
+              offset={-80}
+              duration={600}
+              spy={true}
+              className="cursor-pointer hover:text-indigo-600"
+            >
               Portfolio
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="#contact" className="hover:text-indigo-600">
+            <Link
+              to="contact"
+              smooth={true}
+              offset={-80}
+              duration={600}
+              spy={true}
+              className="cursor-pointer hover:text-indigo-600"
+            >
               Contact
-            </a>
+            </Link>
           </li>
         </ul>
       </nav>
